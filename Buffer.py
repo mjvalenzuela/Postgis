acef5510586edf40ad7e4e13882f3769229b0af2
Johanna <74375508+mjvalenzuela@users.noreply.github.com>
2025-07-03 17:25:19 -0500

    Update Buffer.py

--- Buffer.py
+++ Buffer.py
@@ -2,7 +2,7 @@ from qgis.core import QgsVectorLayer, QgsDataSourceUri
 
 def cargar_capa_sql(nombre_capa, consulta_sql):
     uri = QgsDataSourceUri()
-    uri.setConnection("localhost", "5432", "malla_bogota", "postgres", "homero")
+    uri.setConnection("localhost", "5432", "malla_bogota", "postgres", "postgres")
     uri.setDataSource("", f"({consulta_sql})", "geom", "", "id")
 
     capa = QgsVectorLayer(uri.uri(False), nombre_capa, "postgres")
@@ -11,7 +11,7 @@ def cargar_capa_sql(nombre_capa, consulta_sql):
     else:
         print(f"La capa {nombre_capa} no es válida.")
 
-# Ejemplo: cargar buffers de centros comerciales
+# Ejemplo de crear y mostrar buffers de centros comerciales
 sql = """
 SELECT "id", "NOMBRE", 
        ST_Buffer(geom, 0.005, 'quad_segs=8') AS geom
